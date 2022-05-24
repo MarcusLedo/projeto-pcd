d5a7862264af1a374c609d23e2b02566cc7eeab4
Marcus Ledo <sabaledo27@hotmail.com>
2022-05-24 17:07:05 -0300

    Trying to create an object using a .csv file as source. Async await study needed.

--- application/app.js
+++ application/app.js
@@ -1,15 +1,25 @@
 const csv = require('csv-parser');
 const fs = require('fs');           //File System
-const results = [];                 //Array of objects
+const UFs = [];                 //Array of objects
+const empresas = [];
 
 const uf_path = '/home/marcusledo/Documents/atividade2/CSVs/uf.csv';
 const empresas_path = '/home/marcusledo/Documents/atividade2/CSVs/empresas_bahia.csv';
 const cidade_siafi_path = '/home/marcusledo/Documents/atividade2/CSVs/cidade_siafi.csv';
 const cidade_populacao_path = '/home/marcusledo/Documents/atividade2/CSVs/cidade_populacao.csv';
 
-fs.createReadStream(cidade_populacao_path).pipe(csv()).on('data', (row) => {
+fs.createReadStream(empresas_path).pipe(csv()).on('data', (row) => {
     const properties = Object.getOwnPropertyNames(row);
     console.log(row[properties[0]]);
-})
+    const empresa = {
+        nome_fantasia: row[properties[0]],
+        dt_inicio_atividades: row[properties[1]],
+        cnae_fiscal: row[properties[2]],
+        cep: row[properties[3]],
+        municipio: row[properties[4]]
+    };
 
-console.log(results[0]);
+    empresas.push(empresa);
+});
+
+console.log(empresas);
