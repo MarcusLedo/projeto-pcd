7ba422d11f346bcea22427a980b4e9191501f77a
Marcus Ledo <sabaledo27@hotmail.com>
2022-05-25 20:47:29 -0300

    uf array created

--- application/app.js
+++ application/app.js
@@ -1,9 +1,84 @@
+function nextElem(array){
+    let i = 0;
+    const interator = {
+        next: () => {
+            let element = array[i];
+            i++;
+            return element;
+        }
+    }
+
+    return interator;
+}
+
+function getNameUf(cod){
+    switch(cod){
+        case '11':
+            return 'Rondônia'
+        case '12':
+            return 'Acre';
+        case '13':
+            return 'Amazonas';
+        case '14':
+            return 'Roraima';
+        case '15':
+            return 'Pará';
+        case '16':
+            return 'Amapá';
+        case '17':
+            return 'Tocantins';
+        case '21':
+            return 'Maranhão';
+        case '22':
+            return 'Piauí';
+        case '23':
+            return 'Ceará';
+        case '24':
+            return 'Rio Grande do Norte';
+        case '25':
+            return 'Paraíba';
+        case '26':
+            return 'Pernambuco';
+        case '27':
+            return 'Alagoas';
+        case '28':
+            return 'Sergipe';
+        case '29':
+            return 'Bahia';
+        case '31':
+            return 'Minas Gerais';
+        case '32':
+            return 'Espírito Santo';
+        case '33':
+            return 'Rio de Janeiro';
+        case '35':
+            return 'São Paulo';
+        case '41':
+            return 'Paraná';
+        case '42':
+            return 'Santa Catarina';
+        case '43':
+            return 'Rio Grande do Sul';
+        case '50':
+            return 'Mato Grosso do Sul';
+        case '51':
+            return 'Mato Grosso';
+        case '52':
+            return 'Goiás';
+        case '53':
+            return 'Distrito Federal';
+    }
+}
+
 const csv = require('csv-parser');
 const fs = require('fs');           //File System
+
 const UFs = [];                 //Array of objects
 const empresas = [];
 const cidades_siafi = [];
 const cidades_populacao = [];
+
+const ufDB = [];
 
 const uf_path = '/home/marcusledo/Documents/atividade2/CSVs/uf.csv';
 const empresas_path = '/home/marcusledo/Documents/atividade2/CSVs/empresas_bahia.csv';
@@ -58,6 +133,19 @@ fs.createReadStream(cidade_populacao_path).pipe(csv()).on('data', (row) => {
 
     cidades_populacao.push(cidade_p);
 }).on('end', () =>{
-    console.log(UFs);
+    const walkerUF = nextElem(UFs);
+    let elementUF = walkerUF.next();
+    let id = 1;
 
+    while(elementUF){
+        const ufObj = {};
+        ufObj.id = id;
+        ufObj.sigla = elementUF.uf;
+        ufObj.nome_uf = getNameUf(elementUF.codigo_uf);
+        ufDB.push(ufObj);
+        id++;
+
+        elementUF = walkerUF.next();
+    }
+    console.log(ufDB);
 })
